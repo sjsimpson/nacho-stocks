edb8b6fd9ebcb7e460f040210c67c188e71a06ec
sjsimpson <spencerjsimpson@gmail.com>
2022-12-14 23:03:20 -0700

    another attempt at cloud run config

--- packages/client/index.js
+++ packages/client/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.static(path.resolve(__dirname, './dist')))
 app.get('/*', (req, res) => {
