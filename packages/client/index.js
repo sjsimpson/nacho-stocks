41130f7db5fdb5f2605ac4dc0af5c0967b7d3b8a
sjsimpson <spencerjsimpson@gmail.com>
2022-12-14 22:30:04 -0700

    modify port for hosting app

--- packages/client/index.js
+++ packages/client/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-const PORT = 80
+const PORT = 8080
 
 app.use(express.static(path.resolve(__dirname, './dist')))
 app.get('/*', (req, res) => {
